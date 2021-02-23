149b578fea4e454c771c8f0fc6d3b219870ea03d
NeerajSati <63944649+NeerajSati@users.noreply.github.com>
2021-02-24 01:19:30 +0530

    Create Find the sum of last N nodes of the linked list.cpp

---
+++ Linked List/Easy/Find the sum of last N nodes of the linked list.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+
+void append(struct Node** headRef, int newData)
+{
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	struct Node *last = *headRef;
+	new_node->data = newData;
+	new_node->next = NULL;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+
+int sumOfLastN_Nodes(struct Node* head, int n);
+
+// Driver program to test above
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        struct Node* head = NULL;
+        int n,m,tmp;
+        cin>>m>>n;
+        while(m--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        cout<<sumOfLastN_Nodes(head, n)<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of the node of the linled list is as
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+*/
+// your task is to complete this function 
+// function should return sum of last n nodes
+int sumOfLastN_Nodes(struct Node* head, int n)
+{
+      // Code here
+      int len=0,curr,sum=0;
+      struct Node *temp=head;
+      if(n<1)
+      return 0;
+      while(temp!=NULL)
+      {
+          len++;
+          temp=temp->next;
+      }
+      curr=len-n;
+      temp=head;
+      while(temp!=NULL&&curr--)
+      {
+          temp=temp->next;
+      }
+      while(temp!=NULL)
+      {   sum+=temp->data;
+          temp=temp->next;
+      }
+      return sum;
+      
+}
