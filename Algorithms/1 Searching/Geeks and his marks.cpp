c8adc153e2a1a535156ba3471bb38eecdf61f1d6
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-05-12 11:42:49 +0530

    Create Geeks and his marks.cpp

---
+++ Algorithms/1 Searching/Geeks and his marks.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	//code
+	int t,n,x,low,high,mid,ans;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>x;
+	    int arr[n];
+	    for (int i=0;i<n;i++)
+	    cin>>arr[i];
+	    sort(arr,arr+n);
+	    
+	    high=n-1;
+	    low=0;
+	    ans=n;
+	    
+	    while(low<=high)
+	    {
+	        mid=(low+high)/2;
+	        if(arr[mid]<=x)
+	            low=mid+1;
+	            
+	        else
+	        {
+	            high=mid-1;
+	            ans=mid;
+	        }
+	        
+	    }
+	    cout<<n-ans<<endl;
+	}
+	return 0;
+}
