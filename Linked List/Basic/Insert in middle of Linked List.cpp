f62370091ad2d71b5b60167512ceca21b417fc83
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-06-14 13:37:46 +0530

    Create Insert in middle of Linked List.cpp

---
+++ Linked List/Basic/Insert in middle of Linked List.cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+Node* insertInMiddle(Node* head, int x)
+{
+    Node *temp=new Node(x);
+    if(head==NULL)
+    {
+        return temp;
+    }
+    Node *curr= head;
+    Node *currhalf= head;
+    while(curr->next != NULL && curr->next->next != NULL)
+    {
+        currhalf = currhalf->next;
+        curr = curr->next->next;
+    }
+    temp->next = currhalf->next;
+    currhalf->next = temp;
+    return head;
+}
+
+
