a0360e9ceb9aaca086b638d38458fdc475c14d95
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-06-15 19:28:42 +0530

    Create Rotate Doubly Linked List.cpp

---
+++ Linked List/Easy/Rotate Doubly Linked List.cpp
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+struct node* update(struct node*start,int p);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+        struct node*start = NULL;
+        struct node* cur = NULL;
+        struct node* ptr = NULL;
+        
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            ptr=new node(a);
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=NULL;
+            if(start==NULL)
+            {
+                start=ptr;
+                cur=ptr;
+            }
+            else
+            {
+                cur->next=ptr;
+                ptr->prev=cur;
+                cur=ptr;
+            }
+        }
+        struct node*str=update(start,p);
+        while(str!=NULL)
+        {
+            cout<<str->data<<" ";
+            str=str->next;
+        }
+        cout<<endl;  
+    }
+}
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+*/
+
+struct node*update(struct node*start,int p)
+{
+    //Add your code here
+    if(p==0)
+    return start;
+    
+    node *temp = start;
+    while(temp->next!=NULL)
+    temp = temp->next;
+    start->prev = temp;
+    temp ->next = start;
+    for(int i=0;i<p;i++)
+    temp = temp->next;
+    start = temp -> next;
+    temp->next->prev = NULL;
+    temp->next = NULL;
+    return start;
+}
+
+// { Driver Code Starts.
+  // } Driver Code Ends
