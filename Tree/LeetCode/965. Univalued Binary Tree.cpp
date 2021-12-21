c4643e20269b3d763949d762decdc5f8eb9251ac
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-12-22 00:21:27 +0530

    Update 965. Univalued Binary Tree.cpp

--- Tree/LeetCode/965. Univalued Binary Tree.cpp
+++ Tree/LeetCode/965. Univalued Binary Tree.cpp
@@ -22,3 +22,29 @@ Constraints:
 The number of nodes in the tree is in the range [1, 100].
 0 <= Node.val < 100
   */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        return checkUnival(root, root->val);
+    }
+    bool checkUnival(TreeNode* root,int data){
+        if(root == NULL){
+            return true;
+        }
+        if(root->val != data){
+            return false;
+        }
+        return checkUnival(root->left,data) && checkUnival(root->right,data);
+    }
+};
