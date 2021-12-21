7eb4875588ef5ed7712c42fc8ba8c5c260002dec
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-12-22 00:25:36 +0530

    Create 94. Binary Tree Inorder Traversal.cpp

--- Tree/LeetCode/94. Binary Tree Inorder Traversal.cpp
+++ Tree/LeetCode/94. Binary Tree Inorder Traversal.cpp
@@ -24,3 +24,28 @@ Constraints:
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        if(root == NULL)
+            return arr;
+        vector<int> v2 = inorderTraversal(root->left);
+        arr.insert(arr.end(), v2.begin(), v2.end());
+        arr.push_back(root->val);
+        v2 = inorderTraversal(root->right);
+        arr.insert(arr.end(), v2.begin(), v2.end());
+        return arr;
+    }
+};
