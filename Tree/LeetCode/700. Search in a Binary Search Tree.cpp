c310dfdf07029c8d759d60aecda9e32fb65bbc03
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-12-26 19:59:12 +0530

    Update 700. Search in a Binary Search Tree.cpp

--- Tree/LeetCode/700. Search in a Binary Search Tree.cpp
+++ Tree/LeetCode/700. Search in a Binary Search Tree.cpp
@@ -36,6 +36,9 @@ root is a binary search tree.
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//recursive
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
@@ -47,5 +50,16 @@ public:
             return searchBST(root->left, val);
         else
             return searchBST(root->right, val);
+    }
+ 
+};
+
+
+//Iterative
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root && root->val != val) root = root->val < val ? root->right : root->left;
+	    return root;
     }
 };
