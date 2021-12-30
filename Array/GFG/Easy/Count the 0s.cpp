254889eda11a06135d7e002544a55e3899a24f84
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-12-30 23:33:10 +0530

    Update Count the 0s.cpp

--- Array/GFG/Easy/Count the 0s.cpp
+++ Array/GFG/Easy/Count the 0s.cpp
@@ -12,7 +12,6 @@ using namespace std;
 class Solution{   
 public:
     int countZeroes(int arr[], int n) {
-        // code here
         int count=0;
         for(int i=n-1;arr[i]==0&&i>-1;i--)
         {
