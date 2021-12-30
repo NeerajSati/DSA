833df63570d9b210b432b2da1ab9c2327ebbcbba
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2021-12-30 23:38:43 +0530

    Update 124. Binary Tree Maximum Path Sum.cpp

--- Tree/LeetCode/124. Binary Tree Maximum Path Sum.cpp
+++ Tree/LeetCode/124. Binary Tree Maximum Path Sum.cpp
@@ -4,9 +4,6 @@ A path in a binary tree is a sequence of nodes where each pair of adjacent nodes
 The path sum of a path is the sum of the node's values in the path.
 
 Given the root of a binary tree, return the maximum path sum of any non-empty path.
-
- 
-
 Example 1:
 
 
@@ -19,7 +16,6 @@ Example 2:
 Input: root = [-10,9,20,null,null,15,7]
 Output: 42
 Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
- 
 
 Constraints:
 
