46530ee9e71d5477c9bd81a5ce5f781ddd8d33d0
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-01-13 20:58:08 +0530

    Update 1038. Binary Search Tree to Greater Sum Tree.cpp

--- BST/Leetcode/1038. Binary Search Tree to Greater Sum Tree.cpp
+++ BST/Leetcode/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -6,7 +6,6 @@ As a reminder, a binary search tree is a tree that satisfies these constraints:
 The left subtree of a node contains only nodes with keys less than the node's key.
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
- 
 
 Example 1:
 
