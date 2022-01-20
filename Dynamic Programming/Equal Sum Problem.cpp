55ac877f798813c153a423d9752ace3476703168
Neeraj Sati <neeraj.sati123@gmail.com>
2022-01-20 13:52:29 +0530

    Create Equal Sum Problem.cpp

---
+++ Dynamic Programming/Equal Sum Problem.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool subsetSum (int sum, int val[], int n)
+{
+    int i, j;
+    bool mat[n+1][sum+1];
+    
+    for (int i = 0; i <= n; i++)
+        mat[i][0] = true;
+        
+    for (int i = 1; i <= sum; i++)
+        mat[0][i] = false;
+        
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= sum; j++)
+        {
+            if (val[i - 1] <= j)
+                mat[i][j] = mat[i - 1][j - val[i - 1]] || mat[i - 1][j];
+            else
+                mat[i][j] = mat[i - 1][j];
+        }
+    }
+    return mat[n][sum];
+}
+
+bool equalSum(int val[], int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=val[i];
+    }
+    if(sum%2!=0){
+        return false;
+    }
+    else if(sum%2 == 0){
+        subsetSum(sum/2,val,n);
+    }
+}
+
+int main()
+{
+    int val[] = { 1,5,11,5 };
+    int n = sizeof(val) / sizeof(val[0]);
+     
+    cout << equalSum(val, n);
+     
+    return 0;
+}
+ 
