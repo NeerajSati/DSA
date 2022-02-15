9df855981c3b2d6a3f8e16707bdc78ade19685d4
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-02-15 20:12:28 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- Heap/LeetCode/215. Kth Largest Element in an Array.cpp
+++ Heap/LeetCode/215. Kth Largest Element in an Array.cpp
@@ -24,12 +24,12 @@ Constraints:
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> maxh;
+        priority_queue<int,vector<int>,greater<int>> minh;
         for(int i=0;i<nums.size();i++){
-            maxh.push(nums[i]);
-            if(maxh.size()>k)
-                maxh.pop();
+            minh.push(nums[i]);
+            if(minh.size()>k)
+                minh.pop();
         }
-        return maxh.top();
+        return minh.top();
     }
 };
