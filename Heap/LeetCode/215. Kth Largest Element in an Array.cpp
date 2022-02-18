b4a8c434db80627304c7cb4f78acef333a9e032c
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-02-18 21:23:52 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- Heap/LeetCode/215. Kth Largest Element in an Array.cpp
+++ Heap/LeetCode/215. Kth Largest Element in an Array.cpp
@@ -3,8 +3,6 @@ Given an integer array nums and an integer k, return the kth largest element in 
 
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
- 
-
 Example 1:
 
 Input: nums = [3,2,1,5,6,4], k = 2
