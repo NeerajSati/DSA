67c7baa26e4a947e9bea0d7db812f51afca40e8f
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-11 18:46:49 +0530

    Update 200. Number of Islands.cpp

--- Graph/200. Number of Islands.cpp
+++ Graph/200. Number of Islands.cpp
@@ -66,3 +66,41 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+//or
+class Solution {
+private:
+    bool isPossible(vector<vector<char>> &grid, int x, int y){
+        int m = grid.size(),n = grid[0].size();
+        if(x<m && x>=0 && y<n && y>=0 && grid[x][y] == '1')
+            return true;
+        return false;
+    }
+    void dfs(vector<vector<char>> &grid, int x, int y){
+        if(isPossible(grid,x,y)){
+            grid[x][y] = '0';
+            dfs(grid,x+1,y);
+            dfs(grid,x-1,y);
+            dfs(grid,x,y-1);
+            dfs(grid,x,y+1);
+        }
+           
+}
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                if(grid[i][j] == '1'){
+                    ans++;
+                    dfs(grid,i,j);
+                }
+        return ans;
+    }
+};
