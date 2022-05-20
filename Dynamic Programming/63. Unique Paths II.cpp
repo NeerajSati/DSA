fd70238d6013cf1b851782e231d42e0129d9653b
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-20 23:18:35 +0530

    Update 63. Unique Paths II.cpp

--- Dynamic Programming/63. Unique Paths II.cpp
+++ Dynamic Programming/63. Unique Paths II.cpp
@@ -58,3 +58,28 @@ public:
 //  1 1 1
 //  1 0 1
 //  1 1 2
+
+
+//O(1) space
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1)
+                    grid[i][j] = 0;
+                else if(i==0 && j==0)
+                    grid[i][j] = 1;
+                else if(i==0)
+                    grid[i][j] = grid[i][j-1];
+                else if(j==0)
+                    grid[i][j] = grid[i-1][j];
+                else
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
