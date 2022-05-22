cf9110ac5cd08270a9cf2a734f30c42ece74d8b9
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-22 11:34:20 +0530

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- Tree/LeetCode/116. Populating Next Right Pointers in Each Node.cpp
+++ Tree/LeetCode/116. Populating Next Right Pointers in Each Node.cpp
@@ -76,3 +76,57 @@ public:
         return root;
     }
 };
+
+
+
+//or
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL)
+            return NULL;
+        queue<Node*> q;
+        q.push(root);
+        Node *prev;
+        while(!q.empty()){
+            int size = q.size();
+            bool first = true;
+            for(int i=0;i<size;i++){
+                Node *temp = q.front();
+                q.pop();
+                if(first){
+                    first = false;
+                    prev = temp;
+                }
+                else{
+                    prev->next = temp;
+                    prev = temp;
+                }
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+            prev->next = NULL;
+        }
+        return root;
+    }
+};
