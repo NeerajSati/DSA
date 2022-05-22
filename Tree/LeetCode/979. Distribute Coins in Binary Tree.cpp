fcfc63e65dd767d489339dfa09bc59b183d75156
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-22 18:33:15 +0530

    Create 979. Distribute Coins in Binary Tree.cpp

---
+++ Tree/LeetCode/979. Distribute Coins in Binary Tree.cpp
@@ -0,0 +1,58 @@
+// You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+
+// In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+
+// Return the minimum number of moves required to make every node have exactly one coin.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,0,0]
+// Output: 2
+// Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+// Example 2:
+
+
+// Input: root = [0,3,0]
+// Output: 3
+// Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is n.
+// 1 <= n <= 100
+// 0 <= Node.val <= n
+// The sum of all Node.val is n.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int dfs(TreeNode* root, int &moves){
+        if(root==NULL)
+            return 0;
+        root->val += dfs(root->left, moves) + dfs(root->right, moves);
+        int temp =  root->val - 1;
+        moves += abs(temp);
+        return temp;
+    }
+public:
+    int distributeCoins(TreeNode* root) {
+        int moves = 0;
+        dfs(root,moves);
+        return moves;
+    }
+};
