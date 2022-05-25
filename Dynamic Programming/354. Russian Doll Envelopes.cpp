d88b5b566fc5a3e01d88f6d76386b485b566af6e
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-25 08:48:20 +0530

    Update 354. Russian Doll Envelopes.cpp

--- Dynamic Programming/354. Russian Doll Envelopes.cpp
+++ Dynamic Programming/354. Russian Doll Envelopes.cpp
@@ -33,7 +33,7 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
         int lis[n];
-        int ans = INT_MIN;
+        int ans = 1;
         sort(envelopes.begin(),envelopes.end());
         for(int i=0;i<n;i++){
             int max = 0;
@@ -50,3 +50,52 @@ public:
         return ans;
     }
 };
+
+
+
+
+//O(nlogn)
+
+class Solution {
+private:
+    static bool helper(vector<int> &v1,vector<int> &v2){
+        if(v1[0] < v2[0])
+            return true;
+        else if(v1[0] == v2[0]){
+            return v1[1] > v2[1];
+        }
+        return false;
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        int n = envelopes.size();
+        sort(envelopes.begin(),envelopes.end(),helper);
+        vector<int> dp;
+        for(int i=0;i<n;i++){
+            
+            int currHeight = envelopes[i][1];
+            
+            if(dp.empty() || dp[dp.size() - 1] < currHeight)
+                dp.push_back(currHeight);
+            else{
+                int idx = lower_bound(dp.begin(),dp.end(),currHeight) - dp.begin();
+                dp[idx] = currHeight;
+            }
+        }
+        return dp.size();
+    }
+};
+
+//Suppose array is this
+// 5 4
+// 6 4
+// 6 7
+// 2 3
+
+//We sort array like this
+// 2 3
+// 5 4
+// 6 7
+// 6 4
+
+//And then perform LIS on height
