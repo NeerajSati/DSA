d10c972aa39ceed220cb3846741a6e5a167bdfec
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2022-05-25 09:21:00 +0530

    Create 23. Merge k Sorted Lists.cpp

---
+++ Heap/LeetCode/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,74 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
+ 
+
+// Constraints:
+
+// k == lists.length
+// 0 <= k <= 104
+// 0 <= lists[i].length <= 500
+// -104 <= lists[i][j] <= 104
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length will not exceed 104.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int,ListNode *>,vector<pair<int,ListNode *>>,greater<pair<int,ListNode *>>> minh;
+        for(auto it: lists){
+            if(it){
+                minh.push({it->val,it});       
+            }
+        }
+        ListNode *dummy = new ListNode(), *temp = dummy;
+        while(!minh.empty()){
+            ListNode * curr = minh.top().second;
+            minh.pop();
+            temp->next = curr;
+            temp = temp->next;
+            if(curr -> next){
+                curr = curr->next;
+                minh.push({curr->val,curr});
+            }
+        }
+        
+        
+        return dummy->next;
+    }
+};
